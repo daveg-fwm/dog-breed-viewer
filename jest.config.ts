38724031de321acc410e3371c10889c3e7f93843
Dave Green <wddavegreen@gmail.com>
2026-02-15 21:43:11 +0200

    test: add jest and test file for utility function

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.json",
+      },
+    ],
+  },
+};
